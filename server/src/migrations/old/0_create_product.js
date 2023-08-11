b9bdabb3bd1e44c8bfdd66a24f4088c0391cdbef
haoquachanh <anhhaomnmm@gmail.com>
2023-08-11 16:59:06 +0700

    all

--- server/src/migrations/old/0_create_product.js
+++ server/src/migrations/old/0_create_product.js
@@ -10,73 +10,89 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       avt: {
+        // allowNull: false,
         type: Sequelize.STRING
       },
       video: {
+        // allowNull: false,
         type: Sequelize.STRING
       },
       name: {
+        // allowNull: false,
         type: Sequelize.STRING
       },
       name2: {
+        // allowNull: false,
         type: Sequelize.STRING
       },
       color: {
+        // allowNull: false,
         type: Sequelize.STRING
       },
       colorsys: {
+        // allowNull: false,
         type: Sequelize.STRING
       },
       sides: {
+        // allowNull: false,
         type: Sequelize.STRING
       },
       seen: {
+        // allowNull: false,
         type: Sequelize.INTEGER
       },
       price: {
-        type: Sequelize.DECIMAL(10, 2),
+        // allowNull: false,
+        type: Sequelize.DECIMAL,
       },
       pritech: {
+        // allowNull: false,
         type: Sequelize.STRING
       },
       cut: {
+        // allowNull: false,
         type: Sequelize.STRING
       },
       time: {
+        // allowNull: false,
         type: Sequelize.STRING
       },
       category: {
+        // allowNull: false,
         type: Sequelize.STRING
       },
       material: {
+        // allowNull: false,
         type: Sequelize.STRING
       },
       effect: {
+        // allowNull: false,
         type: Sequelize.STRING
       },
       size: {
+        // allowNull: false,
         type: Sequelize.STRING
       },
       img: {
+        // allowNull: false,
         type: Sequelize.TEXT
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: 0
+        type: Sequelize.DATE
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: 0
+        type: Sequelize.DATE
       }
     });
 
-    const initData = require('../../data/initProduct.json');
-    await queryInterface.sequelize.query('ALTER SEQUENCE products_id_seq RESTART WITH 100000'); // Reset the sequence
+    const initData = require('../../../data/initProduct.json');
+    await queryInterface.sequelize.query('ALTER TABLE Products AUTO_INCREMENT = 100000');
 
     // Insert initial data into the Products table
     await queryInterface.bulkInsert('products', initData, {});
+
   },
 
   down: async (queryInterface, Sequelize) => {
